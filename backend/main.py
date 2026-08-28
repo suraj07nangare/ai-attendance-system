4a368e149630d2668e73a225122f8854acf251ba
Suraj <surajnangare1010@email.com>
2026-08-28 13:20:07 +0530

    removed local

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app = FastAPI(title="AI Attendance API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
+       
         "https://jumpstart-attendance.vercel.app",
     ],
     allow_credentials=True,
